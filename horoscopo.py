be8d57fec1a01f010bab4e761599d9eb2be5534a
Aluno <luhaya@yahoo.com.br>
2023-12-07 10:36:59 -0300

    Commit horoscopo alteracao1

--- horoscopo.py
+++ horoscopo.py
@@ -13,28 +13,28 @@ def signo(mes,dia):
     if (mes == 3 and dia >= 21) or (mes == 4 and dia <= 20):
         print(f'Aries')
     elif (mes == 4 and dia >= 21) or (mes == 5 and dia <= 20):
-        print(f'{mes}Touro')
+        print(f'Touro')
     elif (mes == 5 and dia >= 21) or (mes == 6 and dia <= 20):
-        print(f'{mes}Gêmeos')
+        print(f'Gêmeos')
     elif (mes == 6 and dia >= 21) or (mes == 7 and dia <= 21):
-        print(f'{mes}Cancer')
+        print(f'Cancer')
     elif (mes == 7 and dia >= 23) or (mes == 8 and dia <= 22):
-        print(f'{mes}Leão')
+        print(f'Leão')
     elif (mes == 8 and dia >= 23) or (mes == 9 and dia <= 23):
-        print(f'{mes}Virgem')
+        print(f'Virgem')
     elif (mes == 9 and dia >= 23) or (mes == 10 and dia <= 22):
-        print(f'{mes}Libra')
+        print(f'Libra')
     elif (mes == 10 and dia >= 23) or (mes == 11 and dia <= 22):
-        print(f'{mes}Escorpião')
+        print(f'Escorpião')
     elif (mes == 11 and dia >= 23) or (mes == 12 and dia <= 21):
-        print(f'{mes}Sagitário')
+        print(f'Sagitário')
     elif (mes == 12 and dia >= 22) or (mes == 1 and dia <= 21):
-        print(f'{mes}Capricórnio')
+        print(f'Capricórnio')
     elif (mes == 1 and dia >= 21) or (mes == 2 and dia <= 19):
-        print(f'{mes}Aquário')
+        print(f'Aquário')
     else:
-        print(f'{mes}Peixes')
+        print(f'Peixes')
 
 # Imprimir signo
-signo = signo (mes, dia)
+signo = signo (mes,dia)
 
